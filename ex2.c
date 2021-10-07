0979e27c0597d900cb431de70adece853f21be50
vinayp46 <70538727+vinayp46@users.noreply.github.com>
2021-10-07 17:55:36 -0400

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -12,21 +12,59 @@
 
 /* shared variable for the threads to update */
 volatile int var = WAITING_FOR_SAMPLE;
+pthread_mutex_t lock;
+pthread_cond_t mainC, functionC;
 
-int main(void)
-{
-    uint32_t total_in_arc = 0;
-
-    for (uint32_t i = 0; i < NITERS; i++)
-    {
-        if (circle() == INSIDE_CIRCLE)
-        {
-            total_in_arc++;
+void*
+threadfn(void *arg) {
+    while(1) {
+        // lock this critical area
+        pthread_mutex_lock(&lock);
+        // block mutex to ensure that main called for a signal for functionC
+        while(var != WAITING_FOR_SAMPLE) {
+            pthread_cond_wait(&functionC, &lock);
         }
+        // contintue to here is the signal functionC is sent from main
+        if(var == WAITING_FOR_SAMPLE) {
+            var = circle();
+        }
+        // Unlock and signal back to main
+        pthread_mutex_unlock(&lock);
+        pthread_cond_signal(&mainC);
+    }
+}
+
+
+int main(void) {
+    uint32_t total_in_arc = 0;
+    pthread_t threads[WORKER_COUNT];
+
+    for(int i = 0; i < WORKER_COUNT; i++)  {
+        pthread_create(&threads[i], NULL, &threadfn, NULL);
     }
 
-    /* We multiply by four because circle() uses an 1/4 circular arc */
-    printf("The value of pi is: %f\n", (double)total_in_arc / NITERS * 4);
+    pthread_mutex_init(&lock, NULL);
 
-    return 0;
+	for (uint32_t i = 0; i < NITERS; i++) {
+		// Blocking Mutex lock to wait for var to be filled with some sort of data
+        // uses signaling and waiting to ask fucntion to send data
+		while(var == WAITING_FOR_SAMPLE) {
+            pthread_cond_signal(&functionC);
+            pthread_cond_wait(&mainC, &lock);
+        }
+		// check to see if var is in variable
+		if (var == INSIDE_CIRCLE) {
+			// aggregate the value
+			total_in_arc++;
+		}
+		// Set var back to waiting
+		var = WAITING_FOR_SAMPLE;
+	}
+
+	/* We multiply by four because circle() uses an 1/4 circular arc */
+	printf("The value of pi is: %f\n", (double)total_in_arc / NITERS * 4);
+	//pthread_mutex_unlock(&lock);
+
+
+	return 0;
 }
